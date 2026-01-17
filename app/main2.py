37aa32328004dda485e28d8bf4a985e7bb27c176
Manav Raj <shyro8998@gmail.com>
2026-01-17 22:14:13 +0530

    Add table creation for first deployment

--- app/main2.py
+++ app/main2.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import post, user, auth, votes
-import app.model
-from app.database import engine
+import model
+from database import engine
 
 app = FastAPI()
 
 # Create all tables (UNCOMMENT THIS LINE FOR FIRST DEPLOYMENT)
-app.model.Base.metadata.create_all(bind=engine)
+model.Base.metadata.create_all(bind=engine)
 
 origins = ["*"]
 app.add_middleware(
