e7bfd59c501cc3583a14c5c103730595af1b8766
Manav Raj <shyro8998@gmail.com>
2026-01-17 12:40:49 +0530

    my_changes

--- app/main2.py
+++ app/main2.py
@@ -4,22 +4,10 @@ import model
 from database import engine,get_db
 from routers import post,user,auth,votes
 from fastapi.middleware.cors import CORSMiddleware
-model.Base.metadata.create_all(bind = engine) # ye line hatana hoga when i use alembic ,bcoz alembic will create all database and yahan bas uvicorn se server activate krenge
+#model.Base.metadata.create_all(bind = engine) # ye line hatana hoga when i use alembic ,bcoz alembic will create all database and yahan bas uvicorn se server activate krenge
 app = FastAPI()
 # List of allowed origins (frontend URLs)
-origins = [
-    "http://localhost",          # Localhost without port
-    "http://localhost:3000",     # React default port
-    "http://localhost:5173",     # Vite default port
-    "http://localhost:8080",     # Vue default port
-    "http://127.0.0.1:3000",    # React with IP
-    "http://127.0.0.1:5173",    # Vite with IP
-    # Add your production frontend URL:
-    # "https://yourfrontend.com",
-    # "https://www.yourfrontend.com",
-    "https://www.google.com"
-]
-
+origins = ["*"
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,           # Which origins are allowed
